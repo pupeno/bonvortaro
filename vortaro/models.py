cd261728cc7f58629c1c8b3848fb6f891012f0ca
J. Pablo Fernández <pupeno@pupeno.com>
2008-05-14 20:05:39 +0100

    Added the first model, for words.

--- vortaro/models.py
+++ vortaro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=70)
+    natural_ending = models.CharField(max_length=1)
+    
+    # Not used or unknown.
+    ofc = models.CharField(max_length=1)
+    
+    # Reta-vortaro specific.
+    mrk = models.CharField(max_length=100)
+
+    class Admin:
+        list_display = ("word", "natural_ending")
