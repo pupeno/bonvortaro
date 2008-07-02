fb04e57752a991121ac2fc359b42e2364f3bc422
J. Pablo Fernández <pupeno@pupeno.com>
2008-07-02 22:59:34 +0200

    Set license to be AGPLv3. Since there's no code-downloading facility yet, this should be equivalent as GPLv3.

--- vortaro/management/commands/import_vortaro.py
+++ vortaro/management/commands/import_vortaro.py
@@ -1,22 +1,20 @@
-# Bonvortaro: a good web Esperanto dictionary.
+# Copyright (C) 2008  José Pablo Fernández Silva
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2008  José Pablo Fernández Silva
+# This file is part of Bonvrtaro.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Bonvortaro is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Bonvortaro is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
+# You should have received a copy of the GNU Affero General Public License along
+# with Bonvortaro.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.core.management.base import LabelCommand
 import glob
