2cb97fea2b916d9c5388dd36aae7acc6ce274920
J. Pablo Fernández <pupeno@pupeno.com>
2008-09-26 20:24:02 +0200

    Add unicode representation to words.

--- vortaro/models.py
+++ vortaro/models.py
@@ -55,3 +55,6 @@ class Word(models.Model):
     ofc = models.CharField(max_length=70, blank=True)
     #fnt = models.CharField(max_length=20)
     mrk = models.CharField(max_length=100, blank=True)
+
+    def __unicode__(self):
+        return u"%s %s %s" % (self.begining, self.root.root, self.ending)
