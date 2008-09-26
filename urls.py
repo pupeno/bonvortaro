65a83d0e9c3b1603c398a124a54e493eeb3d820e
J. Pablo Fernández <pupeno@pupeno.com>
2008-09-26 23:36:54 +0200

    Added a non-functional search page and form and everything.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from bonvortaro.vortaro import models
+from bonvortaro.vortaro import views
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.views.generic.list_detail import object_list, object_detail
@@ -21,6 +22,7 @@ urlpatterns = patterns(
     url(r"^root/(?P<object_id>\d+)/$", object_detail, roots, name="root"),
     url(r"^word/$", object_list, words, name="words"),
     url(r"^word/(?P<object_id>\d+)/$", object_detail, words, name="word"),
+    url(r"^search/$", views.search, name="search"),
     
     url(r'^admin/(.*)', admin.site.root, name="admin"),
 )
--- vortaro/views.py
+++ vortaro/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from bonvortaro.vortaro import forms
+
+def search(request):
+    if request.method == 'POST':
+        form = forms.SearchForm(request.POST)
+    else:
+        form = forms.SearchForm(request.GET)
+    return render_to_response("vortaro/search.html", {
+        "form": form
+    })
