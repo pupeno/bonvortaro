bf112f0987064cd391c127329ed0e3f9c407279d
J. Pablo Fernández <pupeno@pupeno.com>
2008-09-26 20:24:42 +0200

    List and display words too.

--- urls.py
+++ urls.py
@@ -9,12 +9,18 @@ roots = {
     "queryset": models.Root.objects.all(),
     "template_object_name": "root"
 }
+words = {
+    "queryset": models.Word.objects.all(),
+    "template_object_name": "word"
+}
 
 urlpatterns = patterns(
     '',
     
     url(r"^root/$", object_list, roots, name="roots"),
     url(r"^root/(?P<object_id>\d+)/$", object_detail, roots, name="root"),
+    url(r"^word/$", object_list, words, name="words"),
+    url(r"^word/(?P<object_id>\d+)/$", object_detail, words, name="word"),
     
     url(r'^admin/(.*)', admin.site.root, name="admin"),
 )
